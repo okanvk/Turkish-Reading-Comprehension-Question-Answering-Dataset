638f4bfd0d73d0956a8f00b42d4f224d9a700c83
ugurcankok <ugurcankok0@gmail.com>
2020-07-10 15:06:26 +0300

    javascript

---
+++ enelpi/main.js
@@ -0,0 +1,22 @@
+
+function myFunction() {
+    let str = "1480 yılında Fatih Sultan Mehmet hayatta iken Osmanlılar İtalya'nın ele geçirilmesi için ilk adım teşkil etmek üzere yarımadanın güneydoğusunda 'çizmenin topuğu' yer alan Otranto kalesini ele geçirmişlerdi. \
+    Fatih'in ölümü ve Şehzade Cem'le II. Bayezid arasındaki taht mücadelesi, İtalya'nın fethi projesinin bir müddet daha ele alınmamasına neden oldu. 1481'de Osmanlı hâkimiyetindeki Otranto kalesi elden çıktı.\
+    Napoli Krallığı, elindeki kuvvetlerle Osmanlı ile başedemeyeceğinin farkındaydı.Ayrıca Osmanlıların İtalya'da bulunmasının krallığın geleceği için iyi olmadığını da biliyordu. O nedenle Napoli Kralı, \
+    damadı Macaristan Kralı Matthias Corvinus'tan ve aynı hanedana mensup bulunduğu, o zamanlar Aragon olarak adlandırılan Kuzey İspanya kralından acele yardım istedi. Macaristan kralının gönderdiği 2 bin atlı ve\
+    diğer İtalyan devletlerinden aldığı yardımcı kuvvetlerle Otranto kalesi önlerine geldi.Bu orduyu denizden Napoli, Papalık ve İspanya gemilerinden müteşekkil bir donanma destekliyordu. \
+    Fatih Sultan Mehmet'in ölüm haberi buraya da ulaşmış ve Osmanlı askerleri arasında büyük bir isteksizlik ortaya çıkmıştı. Tam bu sırada komutan Gedik Ahmet Paşa, yanına aldığı bir miktar asker ve donanma ile ani bir şekilde Otranto'yu terk etti.\
+    Bir rivayete göre bunu kendi kararıyla, bir diğerine göre ise Sultan Bayezid'in isteği ile gerçekleştirmiştir. Gedik Ahmet Paşa Otranto'da 8 bin kadar asker ve asker için 1, 5 yıllık mühimmat bıraktı. \
+    Bu kadar kuvvet ile büyük bir orduya karşı konulması da mümkün değildi. Mukavemet edip 8 bin askeri heba etmek yerine kalenin teslim edilmesine karar verildi.\
+    Osmanlı kuvvetleri, askerlerin tüm silah ve cephanelerini yanlarına alarak çekilmesine izin verilmesi hâlinde, kaleyi teslim edeceklerini taahhüt ettiler.Kaleye yardım gelmesinden korkan Napoli Kralı bu anlaşmayı kabul etti.\
+    Böylece 8 bin Osmanlı askeri tüm mühimmatları ile gemilere binip, Otranto Boğazı'nı geçerek Arnavutluk'ta Osmanlı topraklarına çıktı.\
+    Napoli Kralı, Türkler'in yeniden İtalya'ya çıkmaması için II. Bayezid'in elçisi ile görüştü ve Türkler'in İtalya'ya bir daha sefer düzenlememesi vaadine karşılık Napoli, götürülemeyen Türk toplarını,\
+    Napoli Krallığı içerisindeki bütün Türk ve Müslüman esirleri Osmanlı Devletine geri verdi.Ayrıca dostça olmak şartıyla Donanma - yı Hümayun'a 'Osmanlı Donanması', Adriyatik ve Yunan Denizi'nde serbestçe dolaşma hakkı tanıdı.\
+    Nihayetinde Osmanlı Devleti'nin, İtalya'daki tek kalesi olan Otranto ele geçirilmesinden 13 ay sonra,\
+    10 Eylül 1481'de kaybedildi. Böylece, Fatih Sultan Mehmet tarafından başlatılan İtalya seferi Osmanlı Devletinin iç problemleri sebebiyle durduruldu."
+    
+    
+    
+    let index = str.indexOf("Aragon");
+    document.getElementById("deneme").innerHTML = index;
+}
